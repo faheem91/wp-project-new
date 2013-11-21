dceeab553910d3ba8707d365afb439cb988b30dd
Faheem ud Din <faheem91@outlook.com>
2013-11-21 09:12:09 +0500

    Fixed alumni and search

--- application/controllers/Search/search.php
+++ application/controllers/Search/search.php
@@ -41,39 +41,6 @@ class search extends CI_Controller{
 		}
 		//$this->load->view('common/footer',$data);
 	}
-public function findAlumnai(){
-
-
-$this->load->helper('url');
-
-		if($this->session->userdata('username')){
-				$this->load->model('signup_model');
-		// Validate the user can login
-$id = $this->signup_model->getProfileImage();
-
-				$data["pic_url"]='uploads/30_'.$id.'.jpg';
-				//$another=base_url();
-				//$anotherData='http://'.$another.$data["pic_url"];
-
-				
-
-
-
-			$this->load->view('common/header');
-			$this->load->view('search/find_Alumni.php',$data);
-		}else{
-			$data['msg'] = $msg;
-			$data['heading'] = "Login";
-			$this->load->view('common/header');
-			//$this->load->view('loginView/login_view', $data)
-			$this->load->view('myview/default_main_page.php');
-		}
-
-
-
-
-}
-
 
 public function search_keyword(){
 
@@ -82,6 +49,7 @@ $this->load->helper('url');
 		if($this->session->userdata('username')){
 			$this->load->model('signup_model');
 		// Validate the user can login
+			//ok
 		$id = $this->signup_model->getProfileImage();
 			
 				$data["pic_url"]='uploads/30_'.$id.'.jpg';
