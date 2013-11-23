bee05f4a9b41a9735f46888cc00bd97e3ac31527
Faheem ud Din <faheem91@outlook.com>
2013-11-23 11:57:30 +0500

    Final commit v3

---
+++ application/views/myview/default_main_page - Copy.php
@@ -0,0 +1,145 @@
+﻿content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>World's Largest Professional Network | LinkedIn</title>
+<link rel="shortcut icon" href="images/favicon.ico" />
+</head>
+
+<body>
+	<div class="top">
+	
+		<div class="wrapper">
+			<a href="index.php" class="logo">LinkedIn</a>
+			<ul class="top-navigation">
+			<li><a href="<?php echo base_url('index.php/login/login/what');?>">What is LinkedIn?</a></li>
+			<li><a href="<?php echo base_url('index.php/signup/signup');?>">Join Today</a></li>
+			<li><a href="<?php echo base_url('index.php/login/login/doLogin');?>">Sign In</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="wrapper">
+		<div id="body-area">
+			<div class="padding-0-10px">
+				<div class="main">
+					<div class="content">
+						<h1>Over 225 million professionals use LinkedIn to exchange information, ideas and opportunities</h1>
+						<ul> 
+							<li id="reconnect">Stay informed about your contacts and industry</li>
+							<li id="answers">Find the people &amp; knowledge you need to achieve your goals</li>
+							<li id="power">Control your professional identity online</li>
+						</ul>
+					</div>
+				</div> <!-- END: MAIN -->
+				
+				<div class="sidebar">
+					<div id="entry-form-heading"><h2>Join LinkedIn Today</h2></div>
+					<div class="entry-form">
+						<form action="<?php echo base_url('index.php/signup/signup/signuping');?>" method="post">
+							<label for="fname">First Name:</label><input id="fname" name="firstName" type="text" />
+							<label for="lname">Last Name:</label><input id="lname" name="lastName" type="text" />
+							<label for="email">Email:</label><input id="email" name="email" type="text" />
+							<label for="password">Password:</label><input id="password" name="password" type="password" />
+							<small>6 or more characters</small> <br /><br />
+							<input type="submit" value="Join Now" />
+							<small>Already on LinkedIn? <a href="<?php echo base_url('index.php/login/login/doLogin');?>">Sign in.</a></small>
+						</form>			
+					</div>
+				</div>
+			</div>
+			<div class="clear"></div>
+			<hr />
+			<div id="search-browse">
+				<div id="search-members">
+					<h2>Search for someone by name:</h2>
+					<form>
+						<input type="text" placeholder="First Name" />
+						<input type="text" placeholder="Last Name" />
+						<input type="submit" value="Go" />
+					</form>
+				</div>
+				
+				<p id="member-directory">
+					LinkedIn member directory: 
+					<a href="index.php">a </a>
+					<a href="index.php">b </a>
+					<a href="index.php">c </a>
+					<a href="index.php">d </a>
+					<a href="index.php">e </a>
+					<a href="index.php">f </a>
+					<a href="index.php">g </a>
+					<a href="index.php">h </a>
+					<a href="index.php">i </a>
+					<a href="index.php">j </a>
+					<a href="index.php">k </a>
+					<a href="index.php">l </a>
+					<a href="index.php">m </a>
+					<a href="index.php">n </a>
+					<a href="index.php">o </a>
+					<a href="index.php">p </a>
+					<a href="index.php">q </a>
+					<a href="index.php">r </a>
+					<a href="index.php">s </a>
+					<a href="index.php">t </a>
+					<a href="index.php">u </a>
+					<a href="index.php">v </a>
+					<a href="index.php">w </a>
+					<a href="index.php">x </a>
+					<a href="index.php">y </a>
+					<a href="index.php">z </a>
+					<a href="index.php">more </a>
+						
+					<br />
+						
+					Browse members
+					<a href="index.php"> by country</a>
+				</p>
+			</div>
+			<hr />
+			
+			<p id="statement">
+			*By joining LinkedIn, you agree to LinkedIn's
+			<a href="index.php"> User Agreement</a>,
+			<a href="index.php"> Privacy Policy</a> and
+			<a href="index.php"> Cookie Policy</a>.
+			</p>
+			
+		</div> <!-- END: BODY-AREA -->	
+	</div> <!-- END: WRAPPER -->	
+		
+	<div class="footer">
+		<ul class="footer-links">
+			<li class="bold"><a href="index.php"><strong>Help Center</strong></a></li>
+			<li><a href="index.php">About</a></li>
+			<li><a href="index.php">Press</a></li>
+			<li><a href="index.php">Blog</a></li>
+			<li><a href="index.php">Careers</a></li>
+			<li><a href="index.php">Advertising</a></li>
+			<li><a href="index.php">Talent Solutions</a></li>
+			<li><a href="index.php">Tools</a></li>
+			<li><a href="index.php">Mobile</a></li>
+			<li><a href="index.php">Developers</a></li>
+			<li><a href="index.php">Publishers</a></li>
+			<li><a href="index.php">Language</a></li>
+			<li><a href="index.php">Slideshare</a></li>
+		</ul>
+		<div class="clear"></div>
+		<ul class="footer-links">
+			<li><a href="index.php">LinkedIn Updates</a></li>
+			<li><a href="index.php">LinkedIn Influencers </a></li>
+			<li><a href="index.php">LinkedIn Jobs </a></li>
+			<li><a href="index.php">Jobs Directory </a></li>
+			<li><a href="index.php">Company Directory </a></li>
+			<li><a href="index.php">Groups Directory </a></li>
+			<li><a href="index.php">Title Directory </a></li>
+		</ul>
+		<div class="clear"></div>
+		<ul class="footer-links">
+			<li><img src="images/logo-footer.png" alt="Footer Logo" /></li>
+			<li><a href="index.php">User Agreement </a></li>
+			<li><a href="index.php">Privacy Policy </a></li>
+			<li><a href="index.php">Community Guidelines </a></li>
+			<li><a href="index.php">Cookie Policy </a></li>
+			<li><a href="index.php">Copyright Policy </a></li>
+		</ul>
+	</div> <!-- END: FOOTER -->
+</body>
+
+</html>
