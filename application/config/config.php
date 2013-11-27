a9204ce0f07202af942848ef81fed23e2eb87534
Faheem ud Din <faheem91@outlook.com>
2013-11-27 13:59:19 +0500

    Fixed site address

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://linkedinwp.herokuapp.com/';
+$config['base_url']	= 'http://wp-project-new.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
